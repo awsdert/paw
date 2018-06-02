1d6f7d4533ec9057551a4deb91026d12587cc9fc
awsdert <gb2985@gmail.com>
2018-06-02 10:33:21 +0100

    Since we need to seperate win64 & win32 objects anyways let's just set to use the target in the name, also we switch to making sure paw#.dll is loaded before we try using it. Finally let's use a more sensible name for the test app. Still getting a problem at startup of the exe though.

--- src/win32/pawchk.c
+++ src/win32/pawchk.c
@@ -4,10 +4,25 @@ $(PAW_SRC_DIR)/win32/test.c: $(PAW_INC_DIR)/paw.h
 #	include <paw.h>
 #	include <stdio.h>
 #	include <string.h>
-int main( void ) {
+int CALLBACK WinMain(
+  HINSTANCE hInstance,
+  HINSTANCE hPrevInstance,
+  LPSTR     lpCmdLine,
+  int       nCmdShow
+) {
 	int result = 0;
+#	ifdef _WIN64
+	HMODULE hPaw = LoadLibrary( TEXT("paw64.dll") );
+#	else
+	HMODULE hPaw = LoadLibrary( TEXT("paw32.dll") );
+#	endif
 	puts( "Console Test of paw#.dll" );
 	puts( "Starting..." );
+	if ( !hPaw ) {
+		result = -1;
+		puts( "Failed to load paw#.dll" );
+		goto fail;
+	}
 	pawAPI_t paw = {0};
 	pawAPI( &paw, 0 );
 	if ( !paw.ulBaseAPI ) {
@@ -27,6 +42,8 @@ int main( void ) {
 	done:
 	pawAPI( NULL, 0 );
 	fail:
+	if ( hPaw )
+		FreeLibrary( hPaw );
 	puts( "Exiting..." );
 	return result;
 }
